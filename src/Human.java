0a65657feccf49ee6e7d603d28b3f1acfdcb86b3
olgazaigraewa <ol.zaigraewa@yandex.ru>
2022-11-11 22:34:23 +0300

    Инкапсуляция ДЗ

--- src/Human.java
+++ src/Human.java
@@ -1,42 +1,45 @@
 import java.util.Objects;
 
 public class Human {
-     private final String name;
-     private final String town;
-     private final int yearOfBirth;
-     private String jobTitle;
+       String name;
+     private  String town;
+     private  int yearOfBirth;
+      String jobTitle;
 
      void hello(){
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году.  Я работаю на должности -" + jobTitle + ". Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth +
+                " году.  Я работаю на должности -" + jobTitle + ". Будем знакомы!");
     }
 
 
     public Human(String name, String town, int yearOfBirth,  String jobTitle) {
-         if (name==null){
-             this.name = "Информация не указана";
-         }else {
-             this.name = name;
-         }
-        if (town==null){
-            this.town = "Информация не указана";
-        }else {
-            this.town = town;
-        }
-        if (yearOfBirth<=0){
-            this.yearOfBirth = 0;
-        }else {
-            this.yearOfBirth = yearOfBirth;
-        }
-        if (jobTitle==null){
-            this.jobTitle = "Информация не указана";
-        }else {
-            this.jobTitle = jobTitle;
-        }
+        this.name = Objects.requireNonNullElse(name, "Информация не указана");
+        this.town = Objects.requireNonNullElse(town, "Информация не указана");
+        this.yearOfBirth = Math.max(yearOfBirth, 0);
+        this.jobTitle = Objects.requireNonNullElse(jobTitle, "Информация не указана");
     }
 
     @Override
     public String toString() {
         return "Human" + "name  " + name +  "town " + town + ", yearOfBirth " + yearOfBirth + ", jobTitle " + jobTitle;
+    }
+
+
+
+    public String getTown() {
+         return town;
+    }
+
+    public void setTown(String town) {
+        this.town = Objects.requireNonNullElse(town, "Информация не указана");
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = Math.max(yearOfBirth, 0);
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,123 @@
+import transport.Car;
+
+import java.time.LocalDate;
+
 public class Main {
+
     public static void main(String[] args) {
+        int currentYear = 2022;
 
         Human maksim = new Human(null, "Минск", 1988, "бренд-менеджер");
         maksim.hello();
 
-        Human anya = new Human("Аня",null, 1993, " методист образовательных программ");
+        Human anya = new Human("Аня", null, 1993, " методист образовательных программ");
         anya.hello();
 
         Human katya = new Human("Катя", "Калининград", 0, " продакт-менеджер");
         katya.hello();
 
-        Human artem = new Human("Артём", "Москва",1995, null);
+        Human artem = new Human("Артём", "Москва", 1995, null);
         artem.hello();
 
+        Human wladimir = new Human("Владимир", "Казань", currentYear - 21, "сейчас нигде не работает");
+        wladimir.hello();
+        System.out.println(" ");
 
-        Car lada = new Car(null, " Granta", 2015, "Россия", "желтый", 1.7);
-        System.out.println(lada);
-        Car audi = new Car(" Audi", " A8 50 L TDI quattro", 2020,"Германия", null, 3.0);
-        System.out.println(audi);
-        Car bmw = new Car(" BMW"," Z8", 2021, null,"черный",3.0);
-        System.out.println(bmw);
-        Car kia = new Car(" Kia", " Sportage 4-го поколения", 2018, "Южная Корея","красный", 0 );
-        System.out.println(kia);
+
+        Car lada = new Car("Lada", " Granta", 2015, "Россия", "желтый", 1.7);
+        lada.setTransmission("МКПП");
+        lada.setRegNumber("м256ло459");
+        Car audi = new Car(" Audi", " A8 50 L TDI quattro", 2020, "Германия", null, 3.0);
+        audi.setSummerTires(true);
+        audi.setInsurance(new Car.Insurance(LocalDate.now(), 45_000, "78951236"));
+        audi.getInsurance().checkNumber();
+        Car bmw = new Car(" BMW", " Z8", 2021, null, "черный", 3.0);
+        bmw.setRegNumber("к538во952");
+        bmw.setKey(new Car.Key(true, true));
+        Car kia = new Car(" Kia", " Sportage 4-го поколения", 2018, "Южная Корея", "красный", 0);
         Car hyundai = new Car(" Hyundai ", " Avante", 0, "Южная Корея", "оранжевый", 1.6);
-        System.out.println(hyundai);
+        hyundai.setSummerTires(true);
+        System.out.println(hyundai.isCorrectRegNumber());
+        printInfo(lada);
+        printInfo(audi);
+        printInfo(bmw);
+        printInfo(kia);
+        printInfo(hyundai);
+        System.out.println(" ");
 
+        Flower rosa = new Flower(null, "Голландия", 35.59, 0);
+        Flower chrysanthemum = new Flower(null, null, 15.00, 5);
+        Flower peony = new Flower(null, "Англия", -69.90, 1);
+        Flower gypsophila = new Flower(null, "Турция", 19.50, 10);
+        printInfo(rosa);
+        printInfo(chrysanthemum);
+        printInfo(peony);
+        printInfo(gypsophila);
+        printCostOfFlowers(rosa, rosa, rosa,
+                chrysanthemum, chrysanthemum, chrysanthemum, chrysanthemum, chrysanthemum,
+                gypsophila);
+    }
+
+
+    private static void printInfo(Flower flower) {
+        System.out.println("Цвет: " + flower.getFlowerColor() + ", страна происхождения: " + flower.getCountry() +
+                ", стоимость за штуку: " + flower.getCost() + " рублей , срок стояния: " + flower.lifeSpan + " дней");
+    }
+
+    private static Object printCostOfFlowers(Flower... flowers) {
+        double costOfBouquet = 0;
+        int minimumLifespan = Integer.MAX_VALUE;
+        for (Flower flower : flowers) {
+            if (flower.lifeSpan < minimumLifespan) {
+                minimumLifespan = flower.lifeSpan;
+
+            }
+            costOfBouquet += flower.getCost();
+            printInfo(flower);
+        }
+        costOfBouquet = costOfBouquet * 1.1;
+        System.out.println("Стоимость букета: " + costOfBouquet + " рублей.");
+        System.out.println("Срок стояния букета: " + minimumLifespan + " дней.");
+        return null;
+    }
+
+    private static void printInfo(Car car) {
+        System.out.println(
+                car.getBrand() + " " + car.getModel() +
+                        ", год выпуска - " + car.getYear() + ", сборка -  " + car.getCountry() +
+                        ",  цвет - " + car.getColor() +
+                        ", объем двигателя -  " + car.getEngineVolume() +
+                        " литров, коробка передач - " + car.getTransmission() +
+                        ", тип кузова - " + car.getTypeOfBody() +
+                        ", регистрационный номер - " + car.getRegNumber() +
+                        ", количество мест - " + car.getSeatsCount() +
+                        " , " + (car.isSummerTires() ? "летняя" : "зимняя") + " резина " +
+                        ",  " + (car.getKey().isKeylessAccess() ? "доступ без ключа " : "доступ с ключом") +
+                        ",  " + (car.getKey().isRemoteRunEngine() ? "удаленный запуск " : "обычный запуск") +
+                        ", номер страховки - " + car.getInsurance().getNumber() +
+                        ", стоимость страховки - " + car.getInsurance().getCost() +
+                        ", срок действия страховки - " + car.getInsurance().getExpireDate());
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
